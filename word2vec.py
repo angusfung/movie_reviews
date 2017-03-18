52676039974bc2ab1bb720c65b3a45215f36bd37
Angus <angus.fung@mail.utoronto.ca>
2017-03-18 12:38:13 -0400

    embeddings

---
+++ word2vec.py
@@ -0,0 +1,18 @@
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import time
+from scipy.misc import imread
+from scipy.misc import imresize
+import matplotlib.image as mpimg
+from scipy.ndimage import filters
+import urllib
+from numpy import random
+import os
+import shutil
+import string
+import operator
+import tensorflow as tf
+
+embeddings = load("embeddings.npz")["emb"]
