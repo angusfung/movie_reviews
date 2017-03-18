53e468d7b96678c92f890e9af06a8875881fa6ab
Angus <angus.fung@mail.utoronto.ca>
2017-03-18 00:26:29 -0400

    tuning lambda

--- naivebayes.py
+++ naivebayes.py
@@ -18,9 +18,10 @@ import tensorflow as tf
 
 download   = False #download dataset (just pull from github, don't need to run this)
 run_part2  = False #prints naive bayes classification performance
-run_tuning = False #tuning m and k for naive bayes
+tuning_mk  = False #tuning m and k for naive bayes
 run_part3  = False #printing 10 most important words for negative and positive reviews
-run_part4  = True  #logistic regression via. tensor flow
+run_part4  = False  #logistic regression via. tensor flow
+tuning_lam = True
 
 def download_dataset(neg_index, pos_index, set_type,size): 
     '''param index: list of random, unique numbers
@@ -213,16 +214,11 @@ def most_predictive(neg_dict, pos_dict):
     print(neg_words)
     print(pos_words)
     return neg_words, pos_words
-            
-def get_train(neg_dict, pos_dict):
-    '''param dict {pos, neg} which is a dictionary containing (word, probability)
-       returns:
-            X is a MxN vector, where M is the number of reviews
-                                     N is the number of words 
-            Y is a Mx1 vector, where M is the number of reviews
-    '''
+
+def get_numwords(neg_dict, pos_dict):
+    #return the number of unique words in all the reviews
+    #return a list containing all the words
     
-    global num_words
     combined_list = []
     
     #convert the keys of the dicts into a list
@@ -233,6 +229,19 @@ def get_train(neg_dict, pos_dict):
     #remove duplicates in the list
     combined_list = list(set(combined_list))
     num_words = len(combined_list) 
+    
+    return num_words, combined_list
+            
+def get_train(neg_dict, pos_dict):
+    '''param dict {pos, neg} which is a dictionary containing (word, probability)
+       returns:
+            X is a MxN vector, where M is the number of reviews
+                                     N is the number of words 
+            Y is a Mx1 vector, where M is the number of reviews
+    '''
+    combined_list = []
+    
+    num_words, combined_list = get_numwords(neg_dict, pos_dict)
     
     x = zeros((1600,len(combined_list)))
     y = zeros((1600,2)) #one hot encoding
@@ -270,13 +279,7 @@ def get_test(neg_dict, pos_dict):
 
     combined_list = []
     
-    #convert the keys of the dicts into a list
-    for word in neg_dict:
-        combined_list.append(word)
-    for word in pos_dict:
-        combined_list.append(word)
-    #remove duplicates in the list
-    combined_list = list(set(combined_list))
+    num_words, combined_list = get_numwords(neg_dict, pos_dict)
     
     x = zeros((200,len(combined_list)))
     y = zeros((200,2))
@@ -309,7 +312,44 @@ def get_test(neg_dict, pos_dict):
         #print(cur_row)
     return x, y
     
-def logistic_regression(neg_dict, pos_dict, num_words):
+def get_validation(neg_dict, pos_dict):
+
+    combined_list = []
+    
+    num_words, combined_list = get_numwords(neg_dict, pos_dict)
+    
+    x = zeros((200,len(combined_list)))
+    y = zeros((200,2))
+    cur_row = 0 #keep track of which column we're on
+    
+    path = 'validation_set'
+    
+    for review in os.listdir(path + "\\neg"): 
+        with open(os.getcwd()+"\\"+path+"\\neg\\"+review) as f:
+            unique_words = set(f.read().split())
+            i=0
+            for word in combined_list:
+                if word in unique_words:
+                    x[cur_row, i] = 1
+                    y[cur_row] = [1, 0]
+                i+=1
+        cur_row += 1
+        #print(cur_row)
+        
+    for review in os.listdir(path + "\\pos"): 
+        with open(os.getcwd()+"\\"+path+"\\pos\\"+review) as f:
+            unique_words = set(f.read().split())
+            i=0
+            for word in combined_list:
+                if word in unique_words:
+                    x[cur_row, i] = 1
+                    y[cur_row] = [0, 1]
+                i+=1
+        cur_row += 1
+        #print(cur_row)
+    return x, y
+    
+def logistic_regression(neg_dict, pos_dict, num_words, lam):
     
 
     # Initialize Tensor Flow variables
@@ -320,11 +360,11 @@ def logistic_regression(neg_dict, pos_dict, num_words):
     y = tf.nn.softmax(tf.matmul(x, W0)+b0)
     y_ = tf.placeholder(tf.float32, [None, 2])
     
-    lam = 0.01
+    #lam = 0.01
     decay_penalty =lam*tf.reduce_sum(tf.square(W0))
     reg_NLL = -tf.reduce_sum(y_*tf.log(y))+decay_penalty
     
-    train_step = tf.train.AdamOptimizer(0.005).minimize(reg_NLL)
+    train_step = tf.train.AdamOptimizer(0.0005).minimize(reg_NLL)
     
     init = tf.global_variables_initializer()
     sess = tf.Session()
@@ -334,20 +374,32 @@ def logistic_regression(neg_dict, pos_dict, num_words):
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     
     test_x, test_y = get_test(neg_dict, pos_dict)
+    val_x, val_y = get_validation(neg_dict, pos_dict)
     batch_xs, batch_ys = get_train(neg_dict, pos_dict)
     
+    # Run the TF, collect accuracies in a separate array
+    test_results = []
+    validation_results = []
     for i in range(1000):
     #print i  
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
         
         
-        if i % 1 == 0:
+        if i % 5 == 0:
+            
+            test_accuracy = sess.run(accuracy, feed_dict={x: test_x, y_: test_y})
+            validation_accuracy = sess.run(accuracy, feed_dict={x: val_x, y_: val_y})
+            train_accuracy = sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys})
+            
             print("i=",i)
-            print("Test:", sess.run(accuracy, feed_dict={x: test_x, y_: test_y}))
-            print("Train:", sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys}))
+            print("Test:", test_accuracy)
+            print("Validation:",validation_accuracy)
+            print("Train:", train_accuracy)
             print("Penalty:", sess.run(decay_penalty))
             print(sess.run(reg_NLL, feed_dict = {x: batch_xs, y_: batch_ys}))
-    return
+            test_results.append(test_accuracy)
+            validation_results.append(validation_accuracy)
+    return np.argmax(test_results), np.argmax(validation_results)
 
     
 
@@ -368,6 +420,8 @@ if download == True:
     download_dataset(neg_index[900:], pos_index[900:], "\\validation_set", 200) 
     print("Please wait while the validation set set is being processed...")
 
+
+
 if run_part2 == True:
     '''unnormalized settings''' #normalizing actaully made performance worse...
     dicts = generate_dict('training_set')
@@ -383,7 +437,7 @@ if run_part2 == True:
     score = classify('test_set', neg_dict, pos_dict)
     print(score)
 
-if run_tuning == True:
+if tuning_mk == True:
     max_validation()
     '''I've ran this before and saved the results in a text file called mkscore.
        Highest Performance Tuning:
@@ -399,11 +453,28 @@ if run_part3 == True:
     most_predictive(neg_dict, pos_dict)
 
 if run_part4 == True:
-    dicts = generate_dict('training_set')
-    m = 50
-    k = 50
-    neg_dict = calculate_prob(dicts[0], m, k)
-    pos_dict = calculate_prob(dicts[1], m, k)
-    x,y = get_train(neg_dict, pos_dict)
-    logistic_regression(neg_dict, pos_dict, num_words)
+
+    neg_dict, pos_dict = generate_dict('training_set')
+    num_words = get_numwords(neg_dict, pos_dict)[0]
     
+    lam = 0.01
+    logistic_regression(neg_dict, pos_dict, num_words, lam)
+
+if tuning_lam:
+    
+    neg_dict, pos_dict = generate_dict('training_set')
+    num_words = get_numwords(neg_dict, pos_dict)[0]
+    
+    #store the highest test and validation accuracy
+    highest_test = []
+    highest_validation = []
+    
+    for lam in [0,0.0001,0.0003,0.001,0.003,0.01,0.03,0.1,0.3,1]:
+        results = logistic_regression(neg_dict, pos_dict, num_words, lam)
+        highest_test.append(results[0])
+        highest_validation.append(results[1])
+    
+    print(highest_test)
+    print(highest_validation)
+        
+        
