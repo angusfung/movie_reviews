57e7cf9b380b015c4f6af19c1400218cfdbbb6b2
Oleg Milyutin <omilyutin@me.com>
2017-03-21 15:47:10 -0400

    - non adjacent tuples extracted from text

--- word2vec.py
+++ word2vec.py
@@ -22,8 +22,8 @@ import tensorflow as tf
 # ======================================================================================================================
 
 
-run_part7  = False
-run_part8 = True
+run_part7  = True
+run_part8 = False
 
 
 # ======================================================================================================================
@@ -53,7 +53,7 @@ def word2vec(path, size):
                 word_context['adjacent'].append((text[i], text[i+1]))
                 if total > size/2:
                     break
-                for j in range(i+2, len(text)):
+                for j in range(i+5, len(text)):
                     word_context['non_adjacent'].append((text[i], text[j]))
                     if total_non > size/2:
                         break
@@ -111,7 +111,7 @@ def get_datasets(word_contexts, word_index_dictionary, embeddings, train_size, t
         a = embeddings[word_index_dictionary[word_tuple[0]]]
         b = embeddings[word_index_dictionary[word_tuple[1]]]
         x_train[curr_row, :] = np.concatenate([a, b])
-        y_train = [1, 0]
+        y_train[curr_row] = [1., 0.]
         curr_row += 1
 
     curr_row = 0
@@ -121,7 +121,7 @@ def get_datasets(word_contexts, word_index_dictionary, embeddings, train_size, t
         a = embeddings[word_index_dictionary[word_tuple[0]]]
         b = embeddings[word_index_dictionary[word_tuple[1]]]
         x_test[curr_row, :] = np.concatenate([a, b])
-        y_test = [1, 0]
+        y_test[curr_row] = [1., 0.]
         curr_row += 1
 
     curr_row = 0
@@ -131,7 +131,7 @@ def get_datasets(word_contexts, word_index_dictionary, embeddings, train_size, t
         a = embeddings[word_index_dictionary[word_tuple[0]]]
         b = embeddings[word_index_dictionary[word_tuple[1]]]
         x_validation[curr_row, :] = np.concatenate([a, b])
-        y_validation[curr_row] = [1, 0]
+        y_validation[curr_row] = [1., 0.]
         curr_row += 1
 
     # Fill up the second half - non_adjacent words
@@ -142,7 +142,7 @@ def get_datasets(word_contexts, word_index_dictionary, embeddings, train_size, t
         a = embeddings[word_index_dictionary[word_tuple[0]]]
         b = embeddings[word_index_dictionary[word_tuple[1]]]
         x_train[curr_row, :] = np.concatenate([a, b])
-        y_train = [0, 1]
+        y_train[curr_row] = [0., 1.]
         curr_row += 1
 
     curr_row = test_val_size/2
@@ -152,7 +152,7 @@ def get_datasets(word_contexts, word_index_dictionary, embeddings, train_size, t
         a = embeddings[word_index_dictionary[word_tuple[0]]]
         b = embeddings[word_index_dictionary[word_tuple[1]]]
         x_test[curr_row, :] = np.concatenate([a, b])
-        y_test = [0, 1]
+        y_test[curr_row] = [0., 1.]
         curr_row += 1
 
     curr_row = test_val_size/2
@@ -162,11 +162,11 @@ def get_datasets(word_contexts, word_index_dictionary, embeddings, train_size, t
         a = embeddings[word_index_dictionary[word_tuple[0]]]
         b = embeddings[word_index_dictionary[word_tuple[1]]]
         x_validation[curr_row, :] = np.concatenate([a, b])
-        y_validation[curr_row] = [0, 1]
+        y_validation[curr_row] = [0., 1.]
         curr_row += 1
     working_sets['test'] = (x_test, y_test)
-    working_sets['validation'] = (x_train, y_train)
-    working_sets['train'] = (x_validation, y_validation)
+    working_sets['train'] = (x_train, y_train)
+    working_sets['validation'] = (x_validation, y_validation)
     print("Done constructing the data sets")
     return working_sets
 
@@ -187,8 +187,8 @@ def logistic_regression_part7(word_contexts, word_index_dictionary, embeddings, 
 
     # Initialize Tensor Flow variables
     x = tf.placeholder(tf.float32, [None, 256])
-    W0 = tf.Variable(tf.random_normal([256, 2], stddev=0.01))
-    b0 = tf.Variable(tf.random_normal([2], stddev=0.01))
+    W0 = tf.Variable(tf.random_normal([256, 2], stddev=0.001))
+    b0 = tf.Variable(tf.random_normal([2], stddev=0.001))
 
     y = tf.nn.softmax(tf.matmul(x, W0) + b0)
     y_ = tf.placeholder(tf.float32, [None, 2])
@@ -210,24 +210,24 @@ def logistic_regression_part7(word_contexts, word_index_dictionary, embeddings, 
     val_x, val_y = working_sets['validation']
     batch_xs, batch_ys = working_sets['train']
 
+
     # Run the TF, collect accuracies in separate arrays
     train_results = []
     test_results = []
     validation_results = []
     for i in range(total_iterations):
-        # print i
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-        if i % 3 == 0:
+        if i % 10 == 0:
             test_accuracy = sess.run(accuracy, feed_dict={x: test_x, y_: test_y})
             validation_accuracy = sess.run(accuracy, feed_dict={x: val_x, y_: val_y})
             train_accuracy = sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys})
-            if print_iterations:
-                print("i=", i)
-                print("Train: " + str(train_accuracy))
-                print("Validation: " + str(validation_accuracy))
-                print("Test: "  + str(test_accuracy))
-                print("Penalty: " + str(sess.run(decay_penalty)))
+
+            print("i=", i)
+            print("Train: " + str(train_accuracy))
+            print("Validation: " + str(validation_accuracy))
+            print("Test: "  + str(test_accuracy))
+            print("Penalty: " + str(sess.run(decay_penalty)))
             train_results.append(train_accuracy)
             test_results.append(test_accuracy)
             validation_results.append(validation_accuracy)
@@ -249,11 +249,14 @@ def find_similar(embeddings, word_index_dictionary, word, n):
     return True
 
 if run_part7:
-    train_size = 10000
-    test_val_size = 1000
+    train_size = 150000
+    test_val_size = 15000
+    raw_tuples_number = 100000
+    iters = 250
+    lam = 0.01
 
     embeddings = load("embeddings.npz")["emb"]
-    word_contexts = word2vec('training_set', 20000)
+    word_contexts = word2vec('training_set', raw_tuples_number)
     word_indices = load("embeddings.npz")["word2ind"].flatten()[0]
 
     # Revert the word_indices list to a proper usable format
@@ -283,8 +286,6 @@ if run_part7:
     print("Done sanity check")
 
     # Now, train the LR model
-    iters = 100
-    lam = 0
     results = logistic_regression_part7(word_contexts, word_index_dictionary, embeddings, train_size, test_val_size, lam, iters)
 
     train_results = results[0]
